b92e03a5597683819186042c7dc36fb945f557b7
Force Charlie <ipvb@qq.com>
2020-12-09 13:48:56 +0800

    format cmake files

--- include/bela/time.hpp
+++ include/bela/time.hpp
@@ -684,13 +684,16 @@ template <typename T> T ToChronoDuration(Duration d) {
   using Rep = typename T::rep;
   using Period = typename T::period;
   static_assert(IsValidRep64<Rep>(0), "duration::rep is invalid");
-  if (time_internal::IsInfiniteDuration(d))
+  if (time_internal::IsInfiniteDuration(d)) {
     return d < ZeroDuration() ? (T::min)() : (T::max)();
+  }
   const auto v = ToInt64(d, Period{});
-  if (v > (std::numeric_limits<Rep>::max)())
+  if (v > (std::numeric_limits<Rep>::max)()) {
     return (T::max)();
-  if (v < (std::numeric_limits<Rep>::min)())
+  }
+  if (v < (std::numeric_limits<Rep>::min)()) {
     return (T::min)();
+  }
   return T{v};
 }
 

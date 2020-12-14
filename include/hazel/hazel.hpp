f13d0660b1bbf4c2a87a69c29889ff325d6b3861
Force Charlie <charlieio@outlook.com>
2020-12-14 21:16:07 +0800

    hazel comments

--- include/hazel/hazel.hpp
+++ include/hazel/hazel.hpp
@@ -11,6 +11,64 @@
 
 namespace hazel {
 
+/*
+
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <type_traits>
+#include <variant>
+#include <vector>
+
+// the variant to visit
+using var_t = std::variant<int, long, double, std::string>;
+
+// helper constant for the visitor #3
+template<class> inline constexpr bool always_false_v = false;
+
+// helper type for the visitor #4
+template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+// explicit deduction guide (not needed as of C++20)
+template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+
+int main() {
+    std::vector<var_t> vec = {10, 15l, 1.5, "hello"};
+    for(auto& v: vec) {
+
+        // 1. void visitor, only called for side-effects (here, for I/O)
+        std::visit([](auto&& arg){std::cout << arg;}, v);
+
+        // 2. value-returning visitor, demonstrates the idiom of returning another variant
+        var_t w = std::visit([](auto&& arg) -> var_t {return arg + arg;}, v);
+
+        // 3. type-matching visitor: a lambda that handles each type differently
+        std::cout << ". After doubling, variant holds ";
+        std::visit([](auto&& arg) {
+            using T = std::decay_t<decltype(arg)>;
+            if constexpr (std::is_same_v<T, int>)
+                std::cout << "int with value " << arg << '\n';
+            else if constexpr (std::is_same_v<T, long>)
+                std::cout << "long with value " << arg << '\n';
+            else if constexpr (std::is_same_v<T, double>)
+                std::cout << "double with value " << arg << '\n';
+            else if constexpr (std::is_same_v<T, std::string>)
+                std::cout << "std::string with value " << std::quoted(arg) << '\n';
+            else
+                static_assert(always_false_v<T>, "non-exhaustive visitor!");
+        }, w);
+    }
+
+    for (auto& v: vec) {
+        // 4. another type-matching visitor: a class with 3 overloaded operator()'s
+        std::visit(overloaded {
+            [](auto arg) { std::cout << arg << ' '; },
+            [](double arg) { std::cout << std::fixed << arg << ' '; },
+            [](const std::string& arg) { std::cout << std::quoted(arg) << ' '; },
+        }, v);
+    }
+}
+*/
+
 using hazel_value_t = std::variant<std::string, std::wstring, std::vector<std::string>, std::vector<std::wstring>,
                                    int32_t, int64_t, uint32_t, uint64_t, bela::Time>;
 class hazel_result {

f127c595b3aa46163891080035b96446f37d1c28
Force Charlie <charlieio@outlook.com>
2020-12-20 22:19:51 +0800

    chardet placeholder

---
+++ include/bela/chardet.hpp
@@ -0,0 +1,16 @@
+#ifndef BELA_CHARDET_HPP
+#define BELA_CHARDET_HPP
+#include "base.hpp"
+
+// https://github.com/google/cld3
+
+namespace bela {
+class Universalchardet {
+public:
+  Universalchardet() = default;
+
+private:
+};
+} // namespace bela
+
+#endif

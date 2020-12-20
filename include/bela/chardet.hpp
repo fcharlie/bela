7d9f360f063cd5aeb5ac84c1b4f7f76f5ceb95a2
Force Charlie <charlieio@outlook.com>
2020-12-20 22:46:22 +0800

    Reference

--- include/bela/chardet.hpp
+++ include/bela/chardet.hpp
@@ -3,8 +3,14 @@
 #include "base.hpp"
 
 // Reference
-// https://github.com/google/cld3
+// Encoding detect
+// https://github.com/google/compact_enc_det
+// https://github.com/hsivonen/chardetng
+// Lang detect
 // https://www.greyblake.com/blog/2017-07-30-introduction-to-rust-whatlang-library-and-natural-language-identification-algorithms/
+// https://github.com/greyblake/whatlang-rs
+// https://github.com/google/cld3
+// https://github.com/abadojack/whatlanggo
 
 namespace bela {
 class Universalchardet {

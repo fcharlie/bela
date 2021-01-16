674e2953f1f2e4c5243a18f23b5d2dfa40254831
Force Charlie <charlieio@outlook.com>
2021-01-16 22:42:52 +0800

    StrJoin/StrSplit support std::string

--- test/base/strsplit.cc
+++ test/base/strsplit.cc
@@ -1,4 +1,7 @@
 #include <bela/str_split.hpp>
+#include <bela/str_split_narrow.hpp>
+#include <bela/str_join.hpp>
+#include <bela/str_join_narrow.hpp>
 #include <bela/terminal.hpp>
 
 int wmain() {
@@ -11,7 +14,18 @@ int wmain() {
     for (const auto e : pvv) {
       bela::FPrintF(stderr, L"      %s\n", e);
     }
+    bela::FPrintF(stderr, L"Join: %s\n", bela::StrJoin(pvv, L"/"));
   }
-
+  const char *Apaths[] = {
+      "/////////////Temp/bela/build",
+  };
+  for (const auto p : Apaths) {
+    std::vector<std::string_view> pvv = bela::narrow::StrSplit(p, bela::narrow::ByChar('/'), bela::narrow::SkipEmpty());
+    bela::FPrintF(stderr, L"path: %s\n", p);
+    for (const auto e : pvv) {
+      bela::FPrintF(stderr, L"      %s\n", e);
+    }
+    bela::FPrintF(stderr, L"Join: %s\n", bela::narrow::StrJoin(pvv, "/"));
+  }
   return 0;
 }

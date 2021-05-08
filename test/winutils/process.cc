a89e1b94e3887e3499034693f347863e2154b972
Force Charlie <ipvb@qq.com>
2021-05-08 15:04:43 +0800

    capture process improve

--- test/winutils/process.cc
+++ test/winutils/process.cc
@@ -4,7 +4,7 @@
 int stty_size() {
   bela::process::Process p;
   if (p.CaptureWithMode(bela::process::CAPTURE_USEIN | bela::process::CAPTURE_USEERR, L"stty", L"size") != 0) {
-    bela::FPrintF(stderr, L"%s\n", p.ErrorCode().message);
+    bela::FPrintF(stderr, L"ExitCode: %d Error: %s\nOutput: %s\n", p.ExitCode(), p.ErrorCode().message, p.Out());
     return 1;
   }
   bela::FPrintF(stderr, L"result: %s\n", p.Out());
@@ -17,11 +17,14 @@ int wmain() {
   simulator.InitializeEnv();
   simulator.SetEnv(L"BELA_DEBUG_MODE", L"true");
   bela::process::Process p(&simulator);
-  if (p.Capture(L"vswhere", L"-format", L"json", L"-utf8") == 0) {
+  if (p.CaptureWithMode(bela::process::CAPTURE_USEIN | bela::process::CAPTURE_USEERR, L"vswhere", L"-format", L"json",
+                        L"-utf8") == 0) {
     bela::FPrintF(stderr, L"result: %s\n", p.Out());
+  } else {
+    bela::FPrintF(stderr, L"Exit %s\n", p.ErrorCode().message);
   }
-  bela::FPrintF(stderr, L"\x1b[33mRun cmd\x1b[0m\n");
-  auto exitcode = p.Execute(L"cmd");
-  bela::FPrintF(stderr, L"\x1b[33mcmd exit %d\x1b[0m\n", exitcode);
+  // bela::FPrintF(stderr, L"\x1b[33mRun cmd\x1b[0m\n");
+  // auto exitcode = p.Execute(L"cmd");
+  // bela::FPrintF(stderr, L"\x1b[33mcmd exit %d\x1b[0m\n", exitcode);
   return 0;
 }

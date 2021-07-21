06b977c27b0db6ee5582d9171cf0013176e3fac3
Force Charlie <charlieio@outlook.com>
2021-07-22 03:45:22 +0800

    bela::Buffer cleanup

--- include/bela/buffer.hpp
+++ include/bela/buffer.hpp
@@ -4,12 +4,14 @@
 #include <memory>
 #include <optional>
 #include <span>
+#include <string>
+#include "endian.hpp"
 #include "types.hpp"
 
 namespace bela {
 // Buffer
 namespace buffer {
-template <typename T = uint8_t, typename Allocator = std::allocator<T>> class Buffer {
+template <typename Allocator = std::allocator<uint8_t>> class Buffer {
 private:
   typedef Allocator allocator_type;
   allocator_type get_allocator() const noexcept { return alloc_; }
@@ -28,6 +30,7 @@ private:
       get_allocator().deallocate(data_, capacity_);
       data_ = nullptr;
       capacity_ = 0;
+      size_ = 0;
     }
   }
   allocator_type alloc_;
@@ -52,7 +55,7 @@ public:
     }
     auto b = get_allocator().allocate(n);
     if (size_ != 0) {
-      memcpy(b, data_, sizeof(T) * n);
+      memcpy(b, data_, size_);
     }
     if (data_ != nullptr) {
       get_allocator().deallocate(data_, capacity_);
@@ -60,19 +63,57 @@ public:
     data_ = b;
     capacity_ = n;
   }
+  std::string_view substr(size_t pos = 0) const {
+    if (pos > size_) {
+      return std::string_view();
+    }
+    return std::string_view{reinterpret_cast<const char *>(data_) + pos, size_ - pos};
+  }
+  std::string_view cstring_view(size_t offset, size_t cslength = std::string_view::npos) const {
+    if (offset > size_) {
+      return std::string_view();
+    }
+    cslength = (std::min)(cslength, size_);
+    auto p = data_ + cslength;
+    if (auto end = memchr(data_, 0, cslength); end != nullptr) {
+      return std::string_view(reinterpret_cast<const char *>(p), end - p);
+    }
+    return std::string_view();
+  }
+  template <typename T> const T *direct_cast(size_t offset) const {
+    if (offset + sizeof(T) > size_) {
+      return nullptr;
+    }
+    return reinterpret_cast<const T *>(data_ + offset);
+  }
+  template <typename T> const T *bit_cast(T *t, size_t offset) const {
+    if (offset + sizeof(T) > size_) {
+      return nullptr;
+    }
+    return reinterpret_cast<T *>(memcpy(t, data_ + offset, sizeof(T)));
+  }
+  template <typename T>
+  requires std::integral<T> T cast_fromle(size_t offset)
+  const {
+    if (offset + sizeof(T) > size_) {
+      return 0;
+    }
+    return bela::cast_fromle<T>(data_ + offset);
+  }
+
   template <typename I> [[nodiscard]] const I *cast() const { return reinterpret_cast<const I *>(data_); }
-  [[nodiscard]] const T *data() const { return data_; }
-  [[nodiscard]] T operator[](const size_t _Off) const noexcept { return *(data_ + _Off); }
-  [[nodiscard]] T *data() { return data_; }
-  std::span<T> MakeSpan() const { return std::span(data_, data_ + size_); }
+  [[nodiscard]] const uint8_t *data() const { return data_; }
+  [[nodiscard]] uint8_t operator[](const size_t _Off) const noexcept { return *(data_ + _Off); }
+  [[nodiscard]] uint8_t *data() { return data_; }
+  std::span<uint8_t> MakeSpan() const { return std::span(data_, data_ + size_); }
 
 private:
-  T *data_{nullptr};
+  uint8_t *data_{nullptr};
   size_t size_{0};
   size_t capacity_{0};
 };
 } // namespace buffer
-using Buffer = bela::buffer::Buffer<uint8_t, std::allocator<uint8_t>>;
+using Buffer = bela::buffer::Buffer<std::allocator<uint8_t>>;
 } // namespace bela
 
 #endif

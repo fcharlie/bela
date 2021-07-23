80e4d814f338473ad31b7d573115a9c55f4f477b
Force Charlie <charlieio@outlook.com>
2021-07-23 21:22:40 +0800

    constexpr bela::endian

--- include/bela/endian.hpp
+++ include/bela/endian.hpp
@@ -12,59 +12,67 @@ namespace bela {
 constexpr inline bool IsBigEndian() { return std::endian::native == std::endian::big; }
 constexpr inline bool IsLittleEndian() { return std::endian::native == std::endian::little; }
 
-inline uint16_t swap16(uint16_t value) {
+inline constexpr uint16_t swap16(uint16_t value) {
+  if (std::is_constant_evaluated()) {
+    return (((value & uint16_t{0xFF}) << 8) | ((value & uint16_t{0xFF00}) >> 8));
+  }
 #if defined(_MSC_VER)
   // The DLL version of the runtime lacks these functions (bug!?), but in a
   // release build they're replaced with BSWAP instructions anyway.
   return _byteswap_ushort(value);
 #else
-  uint16_t Hi = value << 8;
-  uint16_t Lo = value >> 8;
-  return Hi | Lo;
+  // defined(__llvm__) || (defined(__GNUC__) && !defined(__ICC))
+  return __builtin_bswap16(src);
 #endif
 }
 // We use C++17. so GCC version must > 8.0. __builtin_bswap32 awayls exists
-inline uint32_t swap32(uint32_t value) {
-#if defined(__llvm__) || (defined(__GNUC__) && !defined(__ICC))
-  return __builtin_bswap32(value);
-#elif defined(_MSC_VER)
+inline constexpr uint32_t swap32(uint32_t value) {
+  if (std::is_constant_evaluated()) {
+    return (((value & uint32_t{0xFF}) << 24) | ((value & uint32_t{0xFF00}) << 8) | ((value & uint32_t{0xFF0000}) >> 8) |
+            ((value & uint32_t{0xFF000000}) >> 24));
+  }
+#if defined(_MSC_VER)
   return _byteswap_ulong(value);
 #else
-  uint32_t Byte0 = value & 0x000000FF;
-  uint32_t Byte1 = value & 0x0000FF00;
-  uint32_t Byte2 = value & 0x00FF0000;
-  uint32_t Byte3 = value & 0xFF000000;
-  return (Byte0 << 24) | (Byte1 << 8) | (Byte2 >> 8) | (Byte3 >> 24);
+  // defined(__llvm__) || (defined(__GNUC__) && !defined(__ICC))
+  return __builtin_bswap32(value);
 #endif
 }
 
-inline uint64_t swap64(uint64_t value) {
-#if defined(__llvm__) || (defined(__GNUC__) && !defined(__ICC))
-  return __builtin_bswap64(value);
-#elif defined(_MSC_VER)
+inline constexpr uint64_t swap64(uint64_t value) {
+  if (std::is_constant_evaluated()) {
+    return (((value & uint64_t{0xFF}) << 56) | ((value & uint64_t{0xFF00}) << 40) |
+            ((value & uint64_t{0xFF0000}) << 24) | ((value & uint64_t{0xFF000000}) << 8) |
+            ((value & uint64_t{0xFF00000000}) >> 8) | ((value & uint64_t{0xFF0000000000}) >> 24) |
+            ((value & uint64_t{0xFF000000000000}) >> 40) | ((value & uint64_t{0xFF00000000000000}) >> 56));
+  }
+#if defined(_MSC_VER)
   return _byteswap_uint64(value);
 #else
-  uint64_t Hi = swap32(uint32_t(value));
-  uint32_t Lo = swap32(uint32_t(value >> 32));
-  return (Hi << 32) | Lo;
+  // defined(__llvm__) || (defined(__GNUC__) && !defined(__ICC))
+  return __builtin_bswap64(value);
 #endif
 }
 
-inline unsigned char bswap(unsigned char v) { return v; }
-inline signed char bswap(signed char v) { return v; }
-inline unsigned short bswap(unsigned short v) { return swap16(v); }
-inline signed short bswap(signed short v) { return static_cast<signed short>(swap16(static_cast<uint16_t>(v))); }
-inline unsigned int bswap(unsigned int v) { return static_cast<unsigned int>(swap32(static_cast<uint32_t>(v))); }
-inline signed int bswap(signed int v) { return static_cast<signed int>(swap32(static_cast<uint32_t>(v))); }
+inline constexpr unsigned char bswap(unsigned char v) { return v; }
+inline constexpr signed char bswap(signed char v) { return v; }
+inline constexpr unsigned short bswap(unsigned short v) { return swap16(v); }
+inline constexpr signed short bswap(signed short v) {
+  return static_cast<signed short>(swap16(static_cast<uint16_t>(v)));
+}
+inline constexpr unsigned int bswap(unsigned int v) {
+  return static_cast<unsigned int>(swap32(static_cast<uint32_t>(v)));
+}
+inline constexpr signed int bswap(signed int v) { return static_cast<signed int>(swap32(static_cast<uint32_t>(v))); }
 
-inline unsigned long bswap(unsigned long v) {
+inline constexpr unsigned long bswap(unsigned long v) {
   static_assert(sizeof(unsigned long) == 8 || sizeof(unsigned long) == 4, "unsigned long size must 8 byte or 4 byte");
   if constexpr (sizeof(unsigned long) == 8) {
     return static_cast<unsigned long>(swap64(static_cast<uint64_t>(v)));
   }
   return static_cast<unsigned long>(swap32(static_cast<uint32_t>(v)));
 }
-inline signed long bswap(signed long v) {
+inline constexpr signed long bswap(signed long v) {
   static_assert(sizeof(signed long) == 8 || sizeof(signed long) == 4, "signed long size must 8 byte or 4 byte");
   if constexpr (sizeof(signed long) == 8) {
     return static_cast<signed long>(swap64(static_cast<uint64_t>(v)));
@@ -72,18 +80,28 @@ inline signed long bswap(signed long v) {
   return static_cast<signed long>(swap32(static_cast<uint32_t>(v)));
 }
 
-inline unsigned long long bswap(unsigned long long v) {
+inline constexpr unsigned long long bswap(unsigned long long v) {
   return static_cast<unsigned long long>(swap64(static_cast<uint64_t>(v)));
 }
 
-inline signed long long bswap(signed long long v) {
+inline constexpr signed long long bswap(signed long long v) {
   return static_cast<signed long long>(swap64(static_cast<uint64_t>(v)));
+}
+
+inline constexpr double bswap(double v) {
+  union {
+    uint64_t i;
+    double d;
+  } in, out;
+  in.d = v;
+  out.i = bswap(in.i);
+  return out.d;
 }
 
 // SO Network order is BigEndian
 template <typename T>
 requires std::integral<T>
-inline T htons(T v) {
+inline constexpr T htons(T v) {
   if constexpr (IsBigEndian()) {
     return v;
   }
@@ -91,7 +109,7 @@ inline T htons(T v) {
 }
 template <typename T>
 requires std::integral<T>
-inline T ntohs(T v) {
+inline constexpr T ntohs(T v) {
   if constexpr (IsBigEndian()) {
     return v;
   }
@@ -100,7 +118,7 @@ inline T ntohs(T v) {
 
 template <typename T>
 requires std::integral<T>
-inline T fromle(T i) {
+inline constexpr T fromle(T i) {
   if constexpr (IsBigEndian()) {
     return bswap(i);
   }
@@ -109,7 +127,7 @@ inline T fromle(T i) {
 
 template <typename T>
 requires std::integral<T>
-inline T frombe(T i) {
+inline constexpr T frombe(T i) {
   if constexpr (IsBigEndian()) {
     return i;
   }
@@ -118,15 +136,23 @@ inline T frombe(T i) {
 
 template <typename T>
 requires std::integral<T>
-inline T unaligned_load(const void *p) {
+inline constexpr T unaligned_load(const void *p) {
   T t;
+  if (std::is_constant_evaluated()) {
+    auto p1 = reinterpret_cast<const uint8_t *>(p);
+    auto p2 = reinterpret_cast<uint8_t *>(&t);
+    for (size_t i = 0; i < sizeof(T); i++) {
+      p2[i] = p1[i];
+    }
+    return t;
+  }
   memcpy(&t, p, sizeof(T));
   return t;
 }
 
 template <typename T>
 requires std::integral<T>
-inline T cast_fromle(const void *p) {
+inline constexpr T cast_fromle(const void *p) {
   auto v = unaligned_load<T>(p);
   if constexpr (IsLittleEndian()) {
     return v;
@@ -136,7 +162,7 @@ inline T cast_fromle(const void *p) {
 
 template <typename T>
 requires std::integral<T>
-inline T cast_frombe(const void *p) {
+inline constexpr T cast_frombe(const void *p) {
   auto v = unaligned_load<T>(p);
   if constexpr (IsBigEndian()) {
     return v;

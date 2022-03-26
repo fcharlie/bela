de349a93e05beba37be38fedbaf6aeda34315688
Force Charlie <charlieio@outlook.com>
2022-03-26 13:44:09 +0800

    Refactor code

--- test/base/base.cc
+++ test/base/base.cc
@@ -7,15 +7,15 @@ int wmain(int argc, wchar_t **argv) {
   if (argc >= 2) {
     FILE *fd = nullptr;
     if (auto e = _wfopen_s(&fd, argv[1], L"rb"); e != 0) {
-      auto ec = bela::make_stdc_error_code(e);
+      auto ec = bela::make_error_code_from_errno(e);
       bela::FPrintF(stderr, L"unable open: %s\n", ec);
       return 1;
     }
     fclose(fd);
   }
   bela::FPrintF(stderr, L"%s\n", bela::narrow::StringCat("H: ", bela::narrow::AlphaNum(bela::narrow::Hex(123456))));
-  bela::FPrintF(stderr, L"EADDRINUSE: %s\nEWOULDBLOCK: %s\n", bela::make_stdc_error_code(EADDRINUSE).message,
-                bela::make_stdc_error_code(EWOULDBLOCK).message);
+  bela::FPrintF(stderr, L"EADDRINUSE: %s\nEWOULDBLOCK: %s\n", bela::make_error_code_from_errno(EADDRINUSE).message,
+                bela::make_error_code_from_errno(EWOULDBLOCK).message);
   auto version = bela::windows::version();
   bela::FPrintF(stderr, L"%d.%d.%d %d.%d\n", version.major, version.minor, version.build, version.service_pack_major,
                 version.service_pack_minor);

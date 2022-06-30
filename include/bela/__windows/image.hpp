c17f5ac9c67c14194be50e5fb85a967ac4c3221b
Force Charlie <6904176+fcharlie@users.noreply.github.com>
2022-06-30 19:21:05 +0800

    PE: Add LoongArch MagicNumber
    See: 
    https://github.com/MicrosoftDocs/win32/pull/1067
    https://go-review.googlesource.com/c/go/+/411616

--- include/bela/__windows/image.hpp
+++ include/bela/__windows/image.hpp
@@ -52,6 +52,8 @@ enum class Machine : uint16_t {
   POWERPC = 0x01F0, // IBM PowerPC Little-Endian
   POWERPCFP = 0x01f1,
   IA64 = 0x0200,      // Intel 64
+  LOONGARCH32 = 0x6232, // LoongArch32
+  LOONGARCH64 = 0x6264, // LongArch64
   MIPS16 = 0x0266,    // MIPS
   ALPHA64 = 0x0284,   // ALPHA64
   MIPSFPU = 0x0366,   // MIPS
